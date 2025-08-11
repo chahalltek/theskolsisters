1a26cf901559f2e37086499fb2e4782e172be1dd
chahalltek <chahall@gmail.com>
2025-08-11 12:13:25 -0400

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,6 +3,9 @@ export default function Page() {
     <div className="container py-12">
       <h1 className="text-3xl md:text-4xl font-bold">About Skol Sisters</h1>
       <p className="text-white/80 mt-2">Women-led fantasy football: smart, sisterly, and zero gatekeeping.</p>
+  <p className="text-white/80 mt-2">I have been playing fantasy football for 30 years and I've been the commish of two leagues of ladies only for 15 years.  If you are interested in learning to play fantasy football or already play and would like to discuss the best strategy for winning your league with other women, we are for you all season.   We all have the team we love to watch.  My team is the Minnesota Vikings.  Skol!   Let's be honest, sometimes our team is in a "rebuilding year" and it can be frustrating to watch.  Fantasy football gives you the opportunity to build a team of players throughout the league.  It makes watching different teams and players so much fun each week.  It adds a whole different layer to those of us that love football.
+In my opinion, there are 5 phases of fantasy football and phase one is about to begin, drafting a winning team.  Every week this month I'll post so of my favorite drafting tips and players to target in your draft.   We will also keep an eye on injuries that happen in the preseason and the inevitable suspensions looming.   Every season is full of surprises and unexpected breakout stars!  Hopefully, we get a few of those players on our team and win our league!
+If you are interested in joining a league, drop me a message and I can organized some leagues!</p>
     </div>
   );
 }
